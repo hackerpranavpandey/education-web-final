55d9f12a80fe5f88cfd4a9b56e5f6b5e540d9a8d
hackerpranavpandey <220010043@iitdh.ac.in>
2024-03-16 19:58:29 +0530

    book.css file

--- src/Component/Pages/Books.js
+++ src/Component/Pages/Books.js
@@ -1,9 +1,54 @@
 import React from 'react'
-
-const Books = () => {
+import '../../book.css'
+function Books() {
   return (
-    <div>
-      Books
+       <div class="button-container">
+        <div class="button">
+            <span>2005</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2006</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2007</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2008</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2009</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2010</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2011</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2012</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2013</span>
+            <span>Previous Year Paper</span>
+        </div>
+
+        <div class="button">
+            <span>2014</span>
+            <span>Previous Year Paper</span>
+        </div>
+        <div class="button">
+            <span>2015</span>
+            <span>Previous Year Paper</span>
+        </div>
+
     </div>
   )
 }
