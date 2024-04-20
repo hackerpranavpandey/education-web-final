f514cf8a462b21ba6b0e48a15572ea6641744102
hackerpranavpandey <220010043@iitdh.ac.in>
2024-04-20 10:59:41 +0530

    Revert "book.css file"
    This reverts commit 55d9f12a80fe5f88cfd4a9b56e5f6b5e540d9a8d.

--- src/Component/Pages/Books.js
+++ src/Component/Pages/Books.js
@@ -1,54 +1,9 @@
 import React from 'react'
-import '../../book.css'
-function Books() {
+
+const Books = () => {
   return (
-       <div class="button-container">
-        <div class="button">
-            <span>2005</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2006</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2007</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2008</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2009</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2010</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2011</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2012</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2013</span>
-            <span>Previous Year Paper</span>
-        </div>
-
-        <div class="button">
-            <span>2014</span>
-            <span>Previous Year Paper</span>
-        </div>
-        <div class="button">
-            <span>2015</span>
-            <span>Previous Year Paper</span>
-        </div>
-
+    <div>
+      Books
     </div>
   )
 }
